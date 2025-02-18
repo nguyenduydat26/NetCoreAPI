f8ba77c14215b6d3dc857f180be08d465bd66101
“nguyenduydat26” <nguyenduydat26072003@gmail.com>
2025-02-19 02:19:16 +0700

    BTH

--- MvcMovie/Program.cs
+++ MvcMovie/Program.cs
@@ -1,27 +1,27 @@
-var builder = WebApplication.CreateBuilder(args);
-
-builder.WebHost.ConfigureKestrel(serverOptions =>
-{
-    serverOptions.ListenLocalhost(5987);
-});
-
+ Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
+app.UseStavar builder = WebApplication.CreateBuilder(args);
+
+//ticFiles();
+
 app.UseRouting();
+
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.Run();
+app.Run();  
